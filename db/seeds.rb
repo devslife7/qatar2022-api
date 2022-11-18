581c2ed9364b4c58d20aaec7b3197f4b31532f94
Marcos Velasco <devslife7@gmail.com>
2022-11-18 14:55:30 -0500

    added User to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create(first_name: "Marcos", last_name: "Velasco", username: "marcos123", password_digest: "password", preditctionsGS: ["A1", "B3", "C2"])
+
+# Creates default users
+# users = [
+#   "Guest", "Marcos", "Andy", "Danny", "Dave", "David", "Shannon", "Solomon",
+#   "Steve", "Yoan", "Zara", "Catrina", "Daniel", "Diego", "Esther", "Issa",
+#   "Kosi", "Paul", "Matine", "Su", "Maddie", "Steven", "Jordan", "Luis",
+#   "Mike", "Princeton", "Shawn", "Vidhi", "Michael", "Ian", "Tien", "Jr",
+#   "Matteo", "Mario", "Esdras", "Pablo", "Briam", "Naod", "Gabriel",
+# ]
+
+# users.each do |user|
+#   name = user.capitalize
+#   username = "#{user.downcase}123"
+#   password = "password"
+
+#   User.create(name: name, username: username, password: password)
+# end
