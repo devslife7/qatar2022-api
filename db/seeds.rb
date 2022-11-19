5ac8e8b2fe7d0f35f915101a7eb1111e1b5d9574
Marcos Velasco <devslife7@gmail.com>
2022-11-18 22:51:52 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,40 @@
 
 User.destroy_all
 
-User.create(first_name: "Marcos", last_name: "Velasco", username: "marcos123", password_digest: "password", predictionsGS: ["A1", "B3", "C2"])
+users = [
+  { first_name: "Marcos", last_name: "Velasco" },
+  { first_name: "Cesar", last_name: "Valensia" },
+  { first_name: "Grabriel", last_name: "Osorio" },
+]
+
+def randomPredictions
+  finalResultsArray = []
+  optionsArray = [
+    "A1", "A2", "A3", "A4",
+    "B1", "B2", "B3", "B4",
+    "C1", "C2", "C3", "C4",
+    "D1", "D2", "D3", "D4",
+    "E1", "E2", "E3", "E4",
+    "F1", "F2", "F3", "F4",
+    "G1", "G2", "G3", "G4",
+    "H1", "H2", "H3", "H4",
+  ]
+
+  48.times do
+    finalResultsArray << optionsArray.sample
+  end
+  finalResultsArray
+end
+
+users.each do |user|
+  predictionsGS = randomPredictions()
+
+  username = "#{user[:first_name].downcase}123"
+
+  User.create(first_name: user[:first_name], last_name: user[:last_name], username: username, password_digest: "password", predictionsGS: predictionsGS)
+end
+
+# User.create(first_name: "Marcos", last_name: "Velasco", username: "marcos123", password_digest: "password", predictionsGS: ["A1", "B3", "C2"])
 
 # User.create(first_name: "Cesar", last_name: "Valencia", username: "cesar123", password_digest: "password", predictionsGS: ["A3", "B2", "C1"])
 
