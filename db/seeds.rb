9e3bddb9801d81daef78ce27ab5f8fed77d1e31d
Marcos Velasco <devslife7@gmail.com>
2022-11-22 14:39:13 -0500

    cleaned up seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -206,53 +206,5 @@ users = [
   },
 ]
 
-def randomPredictions
-  finalResultsArray = []
-  optionsArray = [
-    "A1", "A2", "A3", "A4",
-    "B1", "B2", "B3", "B4",
-    "C1", "C2", "C3", "C4",
-    "D1", "D2", "D3", "D4",
-    "E1", "E2", "E3", "E4",
-    "F1", "F2", "F3", "F4",
-    "G1", "G2", "G3", "G4",
-    "H1", "H2", "H3", "H4",
-  ]
-
-  48.times do
-    finalResultsArray << optionsArray.sample
-  end
-  finalResultsArray
-end
-
-users.each do |user|
-  # predictionsGS = randomPredictions()
-
-  username = "#{user[:first_name].downcase}123"
-
-  User.create(first_name: user[:first_name], last_name: user[:last_name], username: username, password_digest: "password", predictionsGS: user[:predictionsGS])
-end
-
-# User.create(first_name: "Marcos", last_name: "Velasco", username: "marcos123", password_digest: "password", predictionsGS: ["A1", "B3", "C2"])
-
-# User.create(first_name: "Cesar", last_name: "Valencia", username: "cesar123", password_digest: "password", predictionsGS: ["A3", "B2", "C1"])
-
-# Creates default users
-# users = [
-#   "Guest", "Marcos", "Andy", "Danny", "Dave", "David", "Shannon", "Solomon",
-#   "Steve", "Yoan", "Zara", "Catrina", "Daniel", "Diego", "Esther", "Issa",
-#   "Kosi", "Paul", "Matine", "Su", "Maddie", "Steven", "Jordan", "Luis",
-#   "Mike", "Princeton", "Shawn", "Vidhi", "Michael", "Ian", "Tien", "Jr",
-#   "Matteo", "Mario", "Esdras", "Pablo", "Briam", "Naod", "Gabriel",
-# ]
-
-# users.each do |user|
-#   name = user.capitalize
-#   username = "#{user.downcase}123"
-#   password = "password"
-
-#   User.create(name: name, username: username, password: password)
-# end
-
 puts "Users count: #{users.length}"
 puts "Succesfull Seed!!"
