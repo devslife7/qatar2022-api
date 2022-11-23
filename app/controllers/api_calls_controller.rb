037256e2a1738395047990dbe5cca7eb234d4384
Marcos Velasco <devslife7@gmail.com>
2022-11-23 02:37:26 -0500

    enabled fixtures route

--- app/controllers/api_calls_controller.rb
+++ app/controllers/api_calls_controller.rb
@@ -9,28 +9,8 @@ class ApiCallsController < ApplicationController
       :verify_ssl => false,
     )
     data = JSON.parse(response)
-
-    # render json: data["response"]
-
     fixtures = data["response"]
 
     render json: fixtures
-
-    # url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2021-04-19"
-    # headers = { "X-RapidAPI-Key": ENV["API_KEY"] }
-    # response = RestClient::Request.execute(
-    #   :url => url,
-    #   :method => :get,
-    #   :headers => headers,
-    #   :verify_ssl => false,
-    # )
-    # data = JSON.parse(response)
-
-    # # render json: data["response"]
-
-    # fixtures = data["response"].slice(0, 48)
-
-    # render json: fixtures
-
   end
 end
